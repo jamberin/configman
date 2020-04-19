a48bff4855898461858a99df7f998e5c40c222c5
James Beringer <jamberin@gmail.com>
2020-04-19 13:57:34 -0400

    [FLSK] Adding test endpoint

--- application/runner.py
+++ application/runner.py
@@ -6,5 +6,23 @@ This will handle the majority of the functions for the application
 - Authenticate Credential?
 """
 from flask import Flask
+from utils.file_utils import get_default_app_config
+from definitions import get_current_env
 
+# Constants
+CONF = get_default_app_config()
+ENV = get_current_env()
+
+# Flask Setup
 app = Flask(__name__)
+app.secret_key = CONF['configs']['env'][ENV]['configman_key']
+
+
+# Test Endpoint
+@app.route('/test')
+def testing_route():
+    return 'Hello'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
